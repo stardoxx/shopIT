635605f3806dc82edd79b90b838eed2d61addb04
Ashutosh Singh Yadav <ashutoshsinghyadav03@gmail.com>
2021-06-13 02:46:06 +0530

    routing is initialized

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import HomepageComponent from  './HomepageComponent.jsx';
+import HomepageComponent from  './pages/homepage/HomepageComponent.jsx';
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
 import './App.css';
 
 function App() {
+
+const Hats = () =>{
+  return(
+    <div>
+    <h1>hats page</h1>
+  </div>
+  )
+}
+
+
   return (
     <div className="App">
-     <HomepageComponent />
+     <BrowserRouter>
+     <Switch>
+        <Route exact path='/' component={HomepageComponent} />
+        <Route path='/hats' component={Hats} />
+     </Switch>
+     </BrowserRouter>
     </div>
   );
+
 }
 
 export default App;
---
+++ src/pages/homepage/HomepageComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './homepagestyles.scss';
+import Directory from '../../components/directory/DirectoryComponent';
+const HomepageComponent = () => {
+    return (
+      <div className='homepage'>
+      <Directory />
+    </div>
+    )
+}
+
+export default HomepageComponent
